4fa4caa8dc419ca770a82e4aae6e1cf3a3889924
BH4VESH <narolabhavesh480@gmail.com>
2024-06-25 11:30:51 +0530

    build1

--- frontEnd/src/environments/environment.development.ts
+++ frontEnd/src/environments/environment.development.ts
@@ -3,9 +3,9 @@
 //     apiUrl: `13.201.61.84`
     
 // };
-const PORT = 3000
-// const IP = `13.233.110.165`
-const IP = 'localhost'
+const PORT = 3000//3000
+const IP = `13.233.110.165`
+// const IP = 'localhost'
 
 export const environment = {
     stripePublicKey:'pk_test_51PLjvnRvOeoPHU1weSASxnNhkZFs1Lbe2DbSVoT1enRVzrJKRjH2OCCCL9F09sZcTB1bq43QL80zb3vdolC75pF500Zgvg7oCU',
